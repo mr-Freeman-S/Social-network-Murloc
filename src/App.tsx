72af31d82450b0fc67c0260d0ef034758fb271be
mr-Freeman-S <mr.baranok@mail.ru>
2022-01-22 12:58:22 +0300

    18lesson way of samurai, added Dialogs

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,19 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 function App() {
-  return (
-    <div className='app-wrapper'>
-      <Header/>
-        <Navbar/>
-        <Profile/>
-    </div>
-  );
+    return (
+        <div className='app-wrapper'>
+            <Header/>
+            <Navbar/>
+            <div className="app-wrapper-content">
+                <Dialogs />
+            </div>
+            {/*<Profile/>*/}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+function Dialogs(props:any) {
+    return (
+        <div>
+            Dialogs
+        </div>
+    );
+}
+
+export default Dialogs;
