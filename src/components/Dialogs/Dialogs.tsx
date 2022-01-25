bac42287df1fa3fa4e96a0256dd4fa5c3356c38d
mr-Freeman-S <mr.baranok@mail.ru>
2022-01-25 21:41:22 +0300

    25lesson added Array like BLL and use a map() method

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,21 +2,24 @@ import React from "react";
 import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
+
 type MessageType = {
-    message:string
+    message: string
 }
-function Message(props:MessageType) {
-    return(
+
+function Message(props: MessageType) {
+    return (
         <div className={s.message}>{props.message}</div>
     );
 }
 
 type DialogType = {
-    name:string,
-    id:number
+    name: string,
+    id: number
 }
-function Dialog(props:DialogType) {
-    return(
+
+function Dialog(props: DialogType) {
+    return (
         <div className={s.dialog}>
             <NavLink to={"/dialog/" + props.id}>{props.name}</NavLink>
         </div>
@@ -25,21 +28,30 @@ function Dialog(props:DialogType) {
 }
 
 function Dialogs(props: any) {
+    const dialogsData = [
+        {id: 1, name: 'Kate'},
+        {id: 2, name: 'Maks'},
+        {id: 3, name: 'Artem'},
+        {id: 4, name: 'Kostya'},
+        {id: 5, name: 'Sveta'}]
+
+    const messageData = [
+        {id: 1, message: 'How are you?'},
+        {id: 2, message: 'What are you doing now?'},
+        {id: 3, message: 'how are you?'},
+        {id: 4, message: 'Do you want to go to the cinema'}]
+
+    let userDialogs = dialogsData.map(user => <Dialog name={user.name} id={user.id}/>)
+    let userMessages = messageData.map(message => <Message message={message.message}/>)
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <Dialog name={'Kate'} id={1}/>
-                <Dialog name={'Maks'} id={2}/>
-                <Dialog name={'Artem'} id={3}/>
-                <Dialog name={'Kostya'} id={4}/>
-                <Dialog name={'Sveta'} id={5}/>
+                {userDialogs}
 
             </div>
             <div className={s.messages}>
-                <Message message={'How are you?'}/>
-                <Message message={'What are you doing now?'}/>
-                <Message message={'how are you?'}/>
-                <Message message={'how are you?'}/>
+                {userMessages}
             </div>
         </div>
     );
--- src/components/profile/posts/Posts.tsx
+++ src/components/profile/posts/Posts.tsx
@@ -4,15 +4,20 @@ import s from './Posts.module.css'
 
 
 const Posts = () => {
+    const postData = [
+        {id: 1, post: "Hi. My name is Slava", likeCount:3},
+        {id: 2, post: "HI'm first Murlok here", likeCount:5},
+        {id: 3, post: "Do you like Murlok?", likeCount:999},
+        {id: 3, post: "i'm geek", likeCount:-1}]
+
+    let userPosts = postData.map(post => <Post message={post.post} like={post.likeCount}/>)
     return (
     <div className={s.content}><h3>My post:</h3>
         <div>
             <textarea></textarea>
             <div><button>add post</button></div>
         </div>
-        <Post message={"Hi. My name is Slava"} like={3}/>
-        <Post message={"I'm first Murlok here"} like={5}/>
-        <Post message={"Do you like Murlok?"} like={999}/>
+        {userPosts}
     </div>
     );
 }
