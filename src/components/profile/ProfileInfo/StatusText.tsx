1172b7b58c7d068dac2219d1407176be8672a42f
mr-Freeman-S <mr.baranok@mail.ru>
2022-05-07 17:37:39 +0300

    refactor StatusText.tsx add componentDidUpdate

--- src/components/profile/ProfileInfo/StatusText.tsx
+++ src/components/profile/ProfileInfo/StatusText.tsx
@@ -1,31 +1,54 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent} from 'react';
 
-type StatusTextType ={
-    status:string
-    updateStatus:(status:string)=>void
+type StatusTextType = {
+    status: string
+    updateStatus: (status: string) => void
 }
 
-const StatusText = (props:StatusTextType) => {
-    const [editToggle, setEditToggle] = useState<boolean>(false)
-    const [status,setStatus]= useState<string>(props.status)
-    const onDoubleClickHandler = ()=> {
-        setEditToggle(true)
-    }
-    const onBlurHandler = () =>{
-        setEditToggle(false)
-        props.updateStatus(status)
-    }
-    const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setStatus(e.currentTarget.value)
+class StatusText extends React.Component<StatusTextType> {
+    state = {
+        editToggle: false,
+        status: this.props.status
     }
 
-    return (
-        <>
-            {editToggle ?
-                <input autoFocus onBlur={onBlurHandler} onChange={onChangeHandler} value={status} type="text"/> :
-                <span onDoubleClick={onDoubleClickHandler} >{props.status ? props.status: "------"}</span>}
-        </>
-    );
-};
+    onDoubleClickHandler = () => {
+        this.setState({
+            editToggle: true
+        })
+    }
+
+    onBlurHandler = () => {
+        this.setState({
+            editToggle: false
+        })
+        this.props.updateStatus(this.state.status)
+    }
+    onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        this.setState({
+            status: e.currentTarget.value
+        })
+    }
+    componentDidUpdate(prevProps: Readonly<StatusTextType>, prevState: Readonly<{}>, snapshot?: any) {
+        if(prevProps.status !== this.props.status){
+            this.setState({
+                state:this.props.status
+            })
+        }
+    }
+
+    render() {
+
+
+        return (
+            <>
+                {this.state.editToggle ?
+                    <input autoFocus onBlur={this.onBlurHandler} onChange={this.onChangeHandler}
+                           value={this.state.status} type="text"/> :
+                    <span
+                        onDoubleClick={this.onDoubleClickHandler}>{this.props.status ? this.props.status : "------"}</span>}
+            </>
+        );
+    }
+}
 
 export default StatusText;
